56e08cc4f28be8d590028e5cba30ddb60e894620
Ryan Mill <rmill2016@gmail.com>
2023-01-20 10:55:32 -0800

    added icons

--- components/container.js
+++ components/container.js
@@ -1,7 +1,3 @@
 export default function Container({ children }) {
-  return(
-    <div className="px-6 max-w-screen-2xl mx-auto w-full md:px-8">
-      {children}
-    </div>
-  )
+  return <div className="px-6 max-w-screen-2xl mx-auto w-full md:px-8"> {children}</div>
 }
