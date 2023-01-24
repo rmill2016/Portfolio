b3f1398683b42a1b15a1992460b9fb5fb6a34776
Ryan Mill <rmill2016@gmail.com>
2023-01-23 18:41:45 -0800

    update

--- components/header.js
+++ components/header.js
@@ -1,20 +1,166 @@
-import FancyLink from '@/components/fancyLink'
-import Container from '@/components/container'
+import React, { useEffect, useRef, useState } from 'react'
+import { AnimatePresence, motion, useCycle } from 'framer-motion'
 
-export default function Header() {
+const links = [
+  { name: 'Home', to: '/', id: 1 },
+  { name: 'About', to: '#about', id: 2 },
+  { name: 'Services', to: '#services', id: 3 },
+  { name: 'Past Works', to: '#work', id: 4 },
+]
+
+const linkVariants = {
+  closed: {
+    opacity: 0,
+    y: 20,
+  },
+  open: {
+    opacity: 1,
+    y: 0,
+  },
+}
+
+const sideVariants = {
+  closed: {
+    transition: {
+      staggerChildren: 0.15,
+      staggerDirection: -1,
+    },
+  },
+  open: {
+    transition: {
+      staggerChildren: 0.15,
+      delayChildren: 0.3,
+      staggerDirection: 1,
+    },
+  },
+}
+
+const Header = () => {
+  const [open, cycleOpen] = useCycle(false, true)
+  const headerRef = useRef()
+  const headerButtonRef = useRef()
+  const desktopLinks = useRef()
+  const [isDark, setIsDark] = useState(false)
+
+  useEffect(() => {
+    if (open) {
+      document.body.style.overflow = 'hidden'
+    } else document.body.style.overflow = 'visible'
+  }, [open])
+
+  useEffect(() => {
+    let lightSections = document.querySelectorAll('section.bg-white')
+
+    const { top, height } = headerRef.current.getBoundingClientRect()
+
+    let observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          let inverted = false
+          if (entry.isIntersecting) {
+            inverted = true
+          }
+          if (inverted) {
+            desktopLinks.current.classList.remove('text-white')
+            desktopLinks.current.classList.add('text-black')
+            headerButtonRef.current.classList.remove('text-white')
+            headerButtonRef.current.classList.add('text-black')
+            setIsDark(false)
+          } else {
+            desktopLinks.current.classList.remove('text-black')
+            desktopLinks.current.classList.add('text-white')
+            headerButtonRef.current.classList.remove('text-black')
+            headerButtonRef.current.classList.add('text-white')
+            setIsDark(true)
+          }
+        })
+      },
+      {
+        root: document,
+        rootMargin: `-${top + 1}px 0px -${window.innerHeight - top - height}px 0px`,
+      },
+    )
+
+    lightSections.forEach((section) => observer.observe(section))
+  }, [])
+
   return (
-    <header className="py-4 bg-gray-200 mb-4 md:mb-6 xl:mb-8">
-      <Container>
-        <div className="flex flex-wrap">
-          <FancyLink destination="/" a11yText="Navigate to the home page" label="Next x Tailwind x Motion" extraClasses="mb-1 md:mb-0" />
-
-          <nav className="ml-auto flex space-x-3 w-full text-sm md:text-base md:w-auto">
-            <FancyLink destination="/" a11yText="Navigate to the home page" label="Home" />
-
-            <FancyLink destination="/about" a11yText="Navigate to the about page" label="About" />
+    <>
+      <div className="w-full h-screen fixed bg-transparent">
+        <header className="w-full h-[80px] bg-transparent fixed top-0 backdrop-blur-sm z-10" ref={headerRef}>
+          <nav className="flex justify-between items-center w-full h-full lg:hidden">
+            <div className="w-fit h-full">
+              <img
+                src={isDark ? 'assets/logo-light.svg' : 'assets/logo-dark.svg'}
+                alt="logo"
+                className="aspect-video w-fit h-full"
+              />
+            </div>
+            <div className="w-8">
+              <button onClick={cycleOpen}>
+                {open ? <img src="assets/icon-close.svg" alt="close" /> : <img src="assets/icon-bars.svg" alt="menu" />}
+              </button>
+            </div>
           </nav>
-        </div>
-      </Container>
-    </header>
+          <nav className="hidden lg:grid grid-flow-col grid-cols-[200px_1fr_200px]">
+            <div className="w-fit h-full">
+              <img
+                src={isDark ? 'assets/logo-light.svg' : 'assets/logo-dark.svg'}
+                alt="logo"
+                className="aspect-video h-full"
+              />
+            </div>
+            <ul className="list-none inline-flex justify-center items-center gap-8 pl-4 text-white" ref={desktopLinks}>
+              {links.map(({ name, to, id }) => (
+                <a key={id} href={to} variants={linkVariants}>
+                  <li className="link-t-b">{name}</li>
+                </a>
+              ))}
+            </ul>
+            <div className="flex justify-end items-center">
+              <button ref={headerButtonRef} className="filled text-white">
+                Contact
+              </button>
+            </div>
+          </nav>
+        </header>
+        <AnimatePresence>
+          {open && (
+            <motion.aside
+              initial={{ width: 0 }}
+              animate={{
+                width: '40%',
+              }}
+              exit={{
+                width: 0,
+                transition: { delay: 0.7, duration: 0.3 },
+              }}
+              className="absolute bg-white drop-shadow-lg h-screen top-0 left-0 brightness-90 z-20"
+            >
+              <motion.div
+                className="flex flex-col justify-start pt-20 items-start px-8 gap-4 h-full"
+                initial="closed"
+                animate="open"
+                exit="closed"
+                variants={sideVariants}
+              >
+                {links.map(({ name, to, id }) => (
+                  <motion.a className="link-t-b text-2xl" key={id} href={to} variants={linkVariants}>
+                    {name}
+                  </motion.a>
+                ))}
+                <a href="#contact">
+                  <motion.button className="filled" variants={linkVariants}>
+                    Contact
+                  </motion.button>
+                </a>
+              </motion.div>
+            </motion.aside>
+          )}
+        </AnimatePresence>
+      </div>
+    </>
   )
 }
+
+export default Header
--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import { AnimatePresence } from 'framer-motion'
 import { useRouter } from 'next/router'
 import { DefaultSeo } from 'next-seo'
 import SEO from '@/helpers/seo.config'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
   const router = useRouter()
@@ -10,7 +11,14 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <DefaultSeo {...SEO} />
-
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Hind+Madurai&family=Lora:ital,wght@0,400;0,600;1,400&family=Montserrat:ital@0;1&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Head>
       <AnimatePresence mode="wait">
         <Component {...pageProps} key={router.asPath} />
       </AnimatePresence>
