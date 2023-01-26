451b4c66eee59c44e3a91e28ff52ff7d9781b5a4
Ryan Mill <rmill2016@gmail.com>
2023-01-26 14:50:52 -0800

    Tech Stack Section finished

--- components/Tech.jsx
+++ components/Tech.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from 'react'
 import { AnimatePresence, motion, useInView } from 'framer-motion'
 import Carousel from 'framer-motion-carousel'
-import { bounceUp } from '@/helpers/transitions'
+import { bounceUp, staggerDropIn } from '@/helpers/transitions'
 
 let techstackArray = [
   {
@@ -36,6 +36,39 @@ let techstackArray = [
   },
 ]
 
+let frontend = {
+  title: 'Frontend',
+  images: [
+    { src: 'icon-typescript.svg', alt: 'Typescript' },
+    { src: 'icon-javascript.svg', alt: 'Javascript' },
+    { src: 'icon-html.svg', alt: 'HTML' },
+    { src: 'icon-sass.svg', alt: 'Sass' },
+    { src: 'icon-next.svg', alt: 'Next' },
+    { src: 'icon-react.svg', alt: 'React' },
+    { src: 'icon-css.svg', alt: 'CSS' },
+  ],
+}
+
+let backend = {
+  title: 'Backend',
+  images: [
+    { src: 'icon-aws.svg', alt: 'AWS' },
+    { src: 'icon-azure.svg', alt: 'Azure' },
+    { src: 'icon-node.svg', alt: 'Node' },
+    { src: 'icon-firebase.svg', alt: 'Firebase' },
+    { src: 'icon-mongo.svg', alt: 'MongoDB' },
+  ],
+}
+
+let design = {
+  title: 'Design',
+  images: [
+    { src: 'icon-photoshop.svg', alt: 'Adobe Photoshop' },
+    { src: 'icon-framer.svg', alt: 'Framer Motion' },
+    { src: 'icon-figma.svg', alt: 'Figma' },
+  ],
+}
+
 const Tech = () => {
   const container = useRef(null)
   const target = useRef(null)
@@ -48,7 +81,7 @@ const Tech = () => {
       ></div>
       <AnimatePresence>
         <div
-          className="grid grid-cols-1 grid-flow-row place-items-center w-full h-full md:grid-rows-3 md:grid-flow-row"
+          className="grid grid-cols-1 grid-flow-row place-items-center w-full h-full md:grid-rows-[100px_1fr_1fr] md:grid-flow-row md:pt-20"
           ref={target}
         >
           <motion.h3
@@ -98,6 +131,83 @@ const Tech = () => {
               ))}
             </Carousel>
           </div>
+          <motion.div
+            className="flex w-full justify-between items-center h-fit max-w-screen-lg mx-auto -mb-20 lg:-mb-48"
+            variants={staggerDropIn}
+            initial="hide"
+            animate={isInView ? 'show' : 'hide'}
+          >
+            <motion.div className="relative w-[300px] aspect-square flex-center lg:w-[350px]" variants={bounceUp}>
+              <img src="assets/hexagon.png" alt="hexagon" />
+              <div className="absolute left-0 right-0 top-14 bottom-14  flex flex-col gap-4">
+                <h3 className="mx-auto w-fit text-teal">{frontend.title}</h3>
+                <div className="grid grid-cols-3 grid-flow-row place-items-center mx-8">
+                  {frontend.images.map((image) => {
+                    return (
+                      <motion.img
+                        src={`assets/${image.src}`}
+                        alt={image.alt}
+                        className="w-14 aspect-square"
+                        whileHover={{ scale: 1.2 }}
+                      />
+                    )
+                  })}
+                </div>
+              </div>
+            </motion.div>
+            <motion.div className="relative w-[300px] aspect-square flex-center lg:w-[350px]" variants={bounceUp}>
+              <img src="assets/hexagon.png" alt="hexagon" />
+              <div className="absolute left-0 right-0 top-14 bottom-14 flex flex-col gap-4">
+                <h3 className="mx-auto w-fit text-teal ">{backend.title}</h3>
+                <div className="grid grid-cols-3 grid-flow-row place-items-center mx-8">
+                  {backend.images.map((image) => {
+                    return (
+                      <motion.img
+                        src={`assets/${image.src}`}
+                        alt={image.alt}
+                        className="w-14 aspect-square"
+                        whileHover={{ scale: 1.2 }}
+                      />
+                    )
+                  })}
+                </div>
+              </div>
+            </motion.div>
+          </motion.div>
+          <motion.div
+            className="grid w-fit h-fit place-items-center relative mx-auto"
+            initial={{ opacity: 0, y: 20 }}
+            animate={
+              isInView
+                ? {
+                    opacity: 1,
+                    y: 0,
+                    transition: {
+                      type: 'spring',
+                      delay: 0.9,
+                      stiffness: 100,
+                    },
+                  }
+                : { opacity: 0, y: 20 }
+            }
+          >
+            <img src="assets/sitting-image.png" alt="sitting image" className="w-full max-w-[350px] lg:max-w-[400px]" />
+            <div className="absolute bottom-0 p-10 h-1/2 w-full flex flex-col items-center gap-4">
+              <h3 className="text-teal">{design?.title}</h3>
+              <div className="grid grid-cols-3 grid-flow-row place-items-center mx-8 gap-4">
+                {design.images.map((image) => {
+                  return (
+                    <motion.img
+                      src={`assets/${image.src}`}
+                      alt={image.alt}
+                      className="w-14 aspect-square"
+                      whileHover={{ scale: 1.2 }}
+                    />
+                  )
+                })}
+              </div>
+            </div>
+          </motion.div>
         </div>
       </AnimatePresence>
     </section>
