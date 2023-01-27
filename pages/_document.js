d6e4323b48e6d915654150a183ce19c1988b86e4
Ryan Mill <rmill2016@gmail.com>
2023-01-26 18:24:30 -0800

    services section started

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default class MyDocument extends Document {
   render() {
@@ -6,8 +7,9 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="icon" href="/favicon.ico" />
+          <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css" />
         </Head>
-        
+
         <body>
           <Main />
           <NextScript />
